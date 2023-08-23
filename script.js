0573a8fb5bde3734b2ac65a0b64ce855c8a24a3f
ganggaswadiaya <ganggaswadiaya@gmail.com>
2023-08-23 23:32:48 +0800

    improve responsive design

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const humberger = document.querySelector('.hamberger');
 const navLink = document.querySelector('.nav-link');
 
 humberger.addEventListener('click', () => {
-  // Yang kita toggle bukan humberger, tapi navLink
-  // Karena navLink yang kita hide dan show
   navLink.classList.toggle('active');
 });
